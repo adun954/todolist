37dbf0ad22f9a1ebf52415c7bd09b33e5a62ed33
adun954 <allendun954@gmail.com>
2022-11-02 20:21:58 -0700

    todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  // Below array destructure syntax is equivalent to
+  
+
+  const {newTodo, setNewTodo} = useState("");
+  const {todos, setTodos} = useState([]);
+
+  const handleNewTodoSubmit = (event) =>{
+    event.preventDefault();
+
+    if (newTolength == 0) {
+
+        return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      complete: false
+    }
+
+    setTodos({... todos, newTodoItem})
+    setNewTodo("");
+};
+
+  const handleNewTodoDelete = (deLIdx) => {
+    // gives current index and item
+    const filteredTodos = todos.filter((todo, i) => {
+      return i != deLIdx;
+    });
+    
+    setTodos(filteredTodos);
+  }
+
+  const handleNewToggleComplete = (idx) => {
+    const updatedTodos = todos.map((todo, i) => {
+      if(idx == i) {
+    return todo.complete = !todo.complete;
+      }
+
+    // To avoid mutating the todo directly
+        // const updatedTodo = { ... todo, complete: !todo.compete};
+        // return updatedTodo,
+      
+	
+    
+
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: "center"}}>
+      <form 
+        onSubmit={(event) => {
+        handleNewTodoSubmit(event);
+      }}
+      >
+        <input 
+        onChange={(event) => {
+          setNewTodo(event.target.value);
+        }}
+        type ="text" />
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+      
+      {todos.map((todo, i) => {
+        return (
+          <div key={i}>
+            <input onChange={(event) => {
+              handleToggleTodoComplete(i);
+            }} checked= {todo.complete} type="checkbox" />
+            <span>{todo.text}</span>
+            <button onClick={(event) => {
+                handleNewTodoDelete(i);
+              }}
+              style={{marginLeft: "10px"}}
+            >
+                Delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
